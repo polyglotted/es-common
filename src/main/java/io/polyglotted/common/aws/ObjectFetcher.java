8bf6945a4f607b214b893be7c5724ff91743425d
Shankar Vasudevan <shankar@steel-eye.com>
2017-11-14 19:24:48 +0000

    fix s3 may be secure - prepare for release

--- src/main/java/io/polyglotted/common/aws/ObjectFetcher.java
+++ src/main/java/io/polyglotted/common/aws/ObjectFetcher.java
@@ -8,7 +8,6 @@ import com.amazonaws.services.s3.model.S3Object;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static io.polyglotted.common.aws.AwsConfig.createS3Client;
 import static io.polyglotted.common.aws.AwsConfig.s3EncryptionClient;
 import static io.polyglotted.common.util.MapperUtil.readToMap;
@@ -28,13 +27,15 @@ public abstract class ObjectFetcher {
         return fetchObject(createS3Client(config), request).getObjectContent();
     }
 
-    public static InputStream fetchSecure(AwsConfig config, String bucket, String key) throws IOException {
-        return fetchObject(s3EncryptionClient(config, fetchCmkId(config, bucket, key)), new GetObjectRequest(bucket, key)).getObjectContent();
+    public static InputStream fetchMayBeSecure(AwsConfig config, String bucket, String key) throws IOException {
+        String cmkId = fetchCmkId(config, bucket, key);
+        AmazonS3 client = (cmkId == null) ? createS3Client(config) : s3EncryptionClient(config, cmkId);
+        return fetchObject(client, new GetObjectRequest(bucket, key)).getObjectContent();
     }
 
     private static String fetchCmkId(AwsConfig config, String bucket, String key) throws IOException {
         ObjectMetadata metadata = fetchObjectMetadata(config, bucket, key);
-        String matDesc = checkNotNull(metadata.getUserMetaDataOf("x-amz-matdesc"), "missing material description");
-        return reqdStr(readToMap(matDesc), "kms_cmk_id");
+        String matDesc = metadata.getUserMetaDataOf("x-amz-matdesc");
+        return matDesc == null ? null : reqdStr(readToMap(matDesc), "kms_cmk_id");
     }
 }
