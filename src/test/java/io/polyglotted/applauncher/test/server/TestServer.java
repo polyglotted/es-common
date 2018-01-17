d7c01ec77dad83e99fbb482ccc3c8463184b2a32
Shankar Vasudevan <shankar@steel-eye.com>
2018-01-17 22:40:05 +0000

    applauncher integrated

---
+++ src/test/java/io/polyglotted/applauncher/test/server/TestServer.java
@@ -0,0 +1,15 @@
+package io.polyglotted.applauncher.test.server;
+
+import io.polyglotted.applauncher.server.Server;
+
+public class TestServer implements Server {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
